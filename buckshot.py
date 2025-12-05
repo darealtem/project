393b9b8d3035e181b91cd1a62b2c9b1f82a8981d
msiuafhsudofg <gross2729@gmail.com>
2025-12-05 09:49:13 -0500

    Update buckshot.py

--- buckshot.py
+++ buckshot.py
@@ -1,1 +1,38 @@
 import random
+
+def Players():
+    player = None
+    print(f"player: {player}")
+    player2 = None
+    print(f"player2: {player2}")
+    
+
+
+def shotgun():
+    chance = random.choice([i/10 for i in range (11)])
+    if chance <= 0.5:
+        print("blank")
+    elif chance >= 0.6:
+        print("live")
+    
+
+    
+'''
+def round():
+    who starts round
+    pvpp
+    items
+    
+
+
+def main():
+    item logic
+    round logic
+    reviving
+    round count
+    countinue/quit
+    shoot self/other player
+    '''
+    
+    
+Players()
