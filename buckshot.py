de7bb6283835caf51bb00c932384b9d5f2ee5b44
darealtem <landon.tull@gmail.com>
2025-12-05 13:09:47 -0500

    Update buckshot.py

--- buckshot.py
+++ buckshot.py
@@ -1,9 +1,11 @@
 import random
 
 def Players():
-    player = "player"
+    player1_name = input("please enter player 1's name. ")
+    player2_name = input("please enter player 2's name. ")
+    player = player1_name
     print(f"player: {player}")
-    player2 = "dealer"
+    player2 = player2_name
     print(f"player2: {player2}")
     
 
@@ -12,19 +14,27 @@ def shotgun():
     chance = random.choice([i/10 for i in range (11)])
     if chance <= 0.5:
         print("blank")
+        return "blank"
     elif chance >= 0.6:
         print("live")
+        return "live"
     
-
+def load_shotgun():
+    num_of_shells = random.randint (4,9)
+    shells = []
+    for _ in range(num_of_shells):
+        shells.append(shotgun())
+    return shells
     
-'''
+"""
 def round():
+    gun = shotgun
     who starts round
     pvpp
     items
     
 
-
+'''
 def main():
     item logic
     round logic
@@ -33,6 +43,6 @@ def main():
     countinue/quit
     shoot self/other player
     '''
-
-    
+"""
+load_shotgun()
 Players()
